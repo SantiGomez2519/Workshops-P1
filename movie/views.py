8ac5fd1930585ed7638a8d5b60b79d1c439f29ba
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-02-27 11:57:28 -0500

    Feature - Statistics chart

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 from .models import Movie
 
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
+
 # Create your views here.
 def home(request):
     # return HttpResponse('<h1>Welcome to Home Page</h1>')
@@ -16,3 +21,47 @@ def home(request):
 def about(request):
     # return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+
+def statistics_view(request):
+    matplotlib.use('Agg')
+    years = Movie.objects.values_list('year', flat=True).distinct().order_by('year') # Obtener años de las películas
+    movie_counts_by_year = {} # Crear un diccionario para almacenar el conteo de películas por año
+    for year in years:
+        if year:
+            movies_in_year = Movie.objects.filter(year=year)
+        else:
+            movies_in_year = Movie.objects.filter(year__isnull=True)
+            year = 'none'
+        count = movies_in_year.count()
+        movie_counts_by_year[year] = count
+
+    bar_width = 0.5
+    bar_spacing = 0.5
+    bar_positions = range(len(movie_counts_by_year))
+
+    # Crear la gráfica
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    
+    # Personalizar la gráfica
+    plt.title('Movies per Year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+
+    # Ajustar el espaciado entre las barras
+    plt.subplots_adjust(bottom=0.3)
+
+    # Guardar la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    # Convertir la gráfica en formato base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode('utf-8')
+
+    # Renderizar la plantilla con la gráfica
+    return render(request, 'statistics.html', {'graphic': graphic})
